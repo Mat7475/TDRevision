110c29954afd6cd3b46b96d330896ab023fd76be
Mathieu <mat.servonnet@gmail.com>
2025-10-15 12:47:32 +0200

    Ajout Front

---
+++ Front/Service/IService.cs
@@ -0,0 +1,24 @@
+﻿namespace Front.Service
+{
+    public interface IService<Tentity, Tidentifier,TKey> : ServiceReadableRepository<Tentity, Tidentifier>, ServiceWriteableRepository<Tentity>, ServiceSearchableRepository<Tentity, TKey>
+    {
+    }
+
+    public interface ServiceReadableRepository<Tentity, Tidentifier>
+    {
+        Task<IEnumerable<Tentity>> GetAllAsync();
+        Task<Tentity> GetByIdAsync(int id);
+    }
+
+    public interface ServiceSearchableRepository<Tentity, TKey>
+    {
+        Task<Tentity> GetByKeyAsync(TKey str);
+    }
+
+    public interface ServiceWriteableRepository<Tentity>
+    {
+        Task AddAsync(Tentity entity);
+        Task UpdateAsync(int id, Tentity entity);
+        Task DeleteAsync(int id );
+    }
+}
---
+++ Front/Service/WebServiceCommande.cs
@@ -0,0 +1,52 @@
+﻿using Front.Models;
+using System.Net.Http.Json;
+
+namespace Front.Service
+{
+    public class WebServiceCommande : IService<Commande, int, string>
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _endpoint;
+
+        public WebServiceCommande(HttpClient httpClient, string endpoint)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("http://localhost:5283/api/");
+            _endpoint = endpoint;
+        }
+
+        public async Task<IEnumerable<Commande>> GetAllAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<Commande>>($"{_endpoint}/GetAll");
+        }
+
+        public async Task<Commande> GetByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Commande>($"{_endpoint}/GetByID/{id}");
+        }
+
+        public async Task UpdateAsync(int id, Commande entity)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"{_endpoint}/Update/{id}", entity);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{_endpoint}/Delete/{id}");
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task<Commande> GetByKeyAsync(string str)
+        {
+            return await _httpClient.GetFromJsonAsync<Commande>($"{_endpoint}/GetByString/{str}");
+        }
+
+        public async Task AddAsync(Commande entity)
+        {
+            var response = await _httpClient.PostAsJsonAsync($"{_endpoint}/Post", entity);
+            response.EnsureSuccessStatusCode();
+            await response.Content.ReadFromJsonAsync<Commande>();
+        }
+    }
+}
